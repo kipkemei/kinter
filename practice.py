31b9e6809301b86caca676f985ff1265a6757b1e
Am Isaac <pkemey@gmail.com>
2018-05-23 01:44:34 +0300

    inline form

--- practice.py
+++ practice.py
@@ -3,12 +3,9 @@ from tkinter import ttk
 
 root = Tk()
 
-Label(root, text="First Name").grid(row=0, sticky=W, padx=4)
-Entry(root).grid(row=0, column=1, sticky=E, pady=4)
+Label(root, text="Description").grid(row=0, sticky=W, column=0)
+Entry(root, width=50).grid(row=0, column=1)
 
-Label(root, text="Last Name").grid(row=1, sticky=W, padx=4)
-Entry(root).grid(row=1, column=1, sticky=E, pady=4)
-
-Button(root, text="Submit").grid(row=3)
+Button(root, text="Submit").grid(row=0, column=8)
 
 root.mainloop()
